c1289a48baf7c0f6b4ee2818ab0732f9f4d99798
a2katD <mousedrw@ya.ru>
2022-08-10 01:53:48 -0700

    rukopop

--- run.py
+++
@@ -1,9 +0,0 @@
-from wsgiref.simple_server import make_server
-
-from a2kat_framework.main import Framework
-from urls import routes, fronts
-
-application = Framework(routes, fronts)
-
-with make_server('', 8080, application) as httpd:
-    httpd.serve_forever()
