cc2ee50072be258e4ebc501eaaf66d0fb7b61ecd
a2katD <mousedrw@ya.ru>
2022-08-22 10:19:34 -0700

    les3

--- a2kat_framework/templator.py
+++ a2kat_framework/templator.py
@@ -1,9 +1,10 @@
-from jinja2 import Template
 import os
+from jinja2 import Template, FileSystemLoader
+from jinja2.environment import Environment
 
 
 def render(template_name, folder='templates', **kwargs):
-    file_path = os.path.join(folder, template_name)
-    with open(file_path, encoding='utf-8') as file:
-        template = Template(file.read())
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    template = env.get_template(template_name)
     return template.render(**kwargs)
